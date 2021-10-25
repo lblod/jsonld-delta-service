f5bab92ba970ef1a2ed0403bce7bebc05f28dfdf
Nordine Bittich <nordine@bittich.be>
2021-10-25 11:33:13 +0200

    bugfix

--- src/main/java/mu/semte/ch/api/kalliope/script/BulkRunner.java
+++ src/main/java/mu/semte/ch/api/kalliope/script/BulkRunner.java
@@ -8,7 +8,6 @@ import mu.semte.ch.lib.utils.SparqlClient;
 import mu.semte.ch.lib.utils.SparqlQueryStore;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
-import org.apache.jena.rdf.model.Model;
 import org.apache.jena.riot.Lang;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -20,9 +19,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.Map;
 import java.util.stream.Stream;
+
+import static org.apache.commons.io.FilenameUtils.getExtension;
+import static org.apache.jena.riot.Lang.TURTLE;
 
 @Component
 @Slf4j
@@ -31,7 +31,7 @@ public class BulkRunner {
   private final SparqlClient sparqlClient;
   private final SparqlQueryStore queryStore;
 
-  @Value("${application.bulk.directory")
+  @Value("${application.bulk.directory}")
   private String applicationBulkDirectory;
 
   public BulkRunner(PersistService persistService, SparqlClient sparqlClient, SparqlQueryStore queryStore) {
@@ -40,18 +40,22 @@ public class BulkRunner {
     this.queryStore = queryStore;
   }
 
-  @Scheduled(initialDelay = 30000, fixedDelay = 60000) // run 30 seconds after startup & every 60 seconds
+  @Scheduled(initialDelay = 1000,
+             fixedDelay = 60000) // run 30 seconds after startup & then every 60 seconds
   public void run() throws IOException {
     var bulkDir = Paths.get(applicationBulkDirectory);
-    if(isDatabaseUp() && Files.exists(bulkDir) && Files.isDirectory(bulkDir)){
+    if (isDatabaseUp() && Files.exists(bulkDir) && Files.isDirectory(bulkDir)) {
       try (Stream<Path> stream = Files.list(bulkDir)) {
-         stream
-                .filter(path -> !Files.isDirectory(path) && Lang.TURTLE.getFileExtensions().contains(FilenameUtils.getExtension(path.getFileName().toString())))
+        stream
+                .peek(path -> log.debug(path.getFileName().toString()))
+                .filter(path -> !Files.isDirectory(path) && TURTLE.getFileExtensions()
+                                                                       .contains(getExtension(path.getFileName()
+                                                                                                  .toString())))
                 .map(Path::toFile)
                 .forEach(f -> {
                   log.info("processing file {}", f.getName());
                   try {
-                    persistService.writeBulk(ModelUtils.toModel(new FileInputStream(f), Lang.TURTLE.getName()), ()-> this.renameFileAfterProcessed(f)); //rename file
+                    persistService.writeBulk(ModelUtils.toModel(new FileInputStream(f), TURTLE.getName()), () -> this.renameFileAfterProcessed(f)); //rename file
                   }
                   catch (IOException e) {
                     log.error("an error occurred while processing file", e);
@@ -63,22 +67,22 @@ public class BulkRunner {
   }
 
   @SneakyThrows
-  private Void renameFileAfterProcessed(File f){
-    FileUtils.moveFile(f, new File(f.getAbsolutePath() +".processed"));
+  private Void renameFileAfterProcessed(File f) {
+    FileUtils.moveFile(f, new File(f.getAbsolutePath() + ".processed"));
     return null;
   }
 
-  private boolean isDatabaseUp(){
+  private boolean isDatabaseUp() {
     try {
-      var dummyResult= sparqlClient.executeSelectQueryAsListMap(queryStore.getQuery("dummyQuery"));
+      var dummyResult = sparqlClient.executeSelectQueryAsListMap(queryStore.getQuery("dummyQuery"));
       log.debug("DB UP");
       return true;
-    }catch (Exception e){
+    }
+    catch (Exception e) {
       log.debug("DB NOT UP", e);
       return false;
     }
   }
-
 
 
 }
